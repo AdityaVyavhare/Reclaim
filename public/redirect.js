a4451cdb84a80eadeebef855e7d8087c7092c17f
AdityaVyavhare <vyavhareaditya11@gmail.com>
2025-05-26 09:38:21 +0530

    aditya

---
+++ public/redirect.js
@@ -0,0 +1,12 @@
+// Get the itemId from the URL
+const itemId = new URLSearchParams(window.location.search).get("itemId");
+
+const appLink = `reclaim://?itemId=${itemId}`;
+const apkLink = "https://expo.dev/artifacts/eas/qxd9uz1J3ENRV9ucgJKu9a.apk";
+// Try to open the app
+window.location.href = appLink;
+
+// If the app is NOT installed, redirect to APK link after 2 seconds
+setTimeout(() => {
+  window.location.href = apkLink;
+}, 2000);
